bfc9eb267402ae4c4f2f3dc16d9959107ad723bf
cathrynsupko <cathryn.supko@gmail.com>
2015-12-08 10:17:30 -0500

    add login to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in(user)
       redirect_to @user
     else
       redirect_to posts_path
